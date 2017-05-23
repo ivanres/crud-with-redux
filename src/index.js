dc923fe84274507ce15e237059b4fa9bb43143cf
Ivan Rodriguez <ivan.rodriguez@jobsity.com>
2017-05-23 12:32:11 -0500

    Setup reducers and middleware: thunk, dev-tools

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(thunk)
+		)
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
